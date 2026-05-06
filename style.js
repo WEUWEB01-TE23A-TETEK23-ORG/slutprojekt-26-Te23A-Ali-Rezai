e36232ef066dab3e4cbcdb134090f9fd1b457ef3
Ali Rezai <ali.rezai2@elev.ga.ntig.se>
2026-05-06 11:05:54 +0200

    Jag är klar och jobbar med dokumenten.

---
+++ style.js
@@ -0,0 +1,8 @@
+let fnamn = "Ali"
+let enamn = "Rezai"
+let older = "29"
+alert(" jag heter " + fnamn + "  " + enamn + " och är " + older + " gammal " )
+console.log("hej där")
+
+let svar = prompt("vad heter du")
+console.log(svar)
